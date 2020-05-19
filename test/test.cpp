16d356dfc2cf65bd1c31629b36eff3154686702d
abdullahdeliogullari <abdullahdeliogullari@localhost.localdomain>
2020-05-19 23:02:56 +0300

    add test des

--- test/test.cpp
+++ test/test.cpp
@@ -8,16 +8,20 @@
 #include "../CaesarCipher/CaesarCipher.h"
 #include "../MonoalphabeticCipher/MonoalphabeticCipher.h"
 #include "../PolyalphabeticCipher/PolyalphabeticCipher.h"
+#include "../DataEncryptionStandard/DataEncryptionStandard.h"
 
 using namespace std;
 
 int caesarRandomKey = rand() % 24;
 int monoalphabeticRandomSeed = rand() % 100;
 string polyalphabeticRandomKey = "secretKey";
+string desRandomKey = "secureKey";
 
 CaesarCipher caesarCipher = CaesarCipher(caesarRandomKey);
 MonoalphabeticCipher monoalphabeticCipher = MonoalphabeticCipher(monoalphabeticRandomSeed);
 PolyalphabeticCipher polyalphabeticCipher = PolyalphabeticCipher(polyalphabeticRandomKey);
+DataEncryptionStandard dataEncryptionStandard = DataEncryptionStandard(desRandomKey);
+DataEncryptionStandard dataEncryptionStandardPermutation = DataEncryptionStandard(desRandomKey);
 
 TEST(CaesarCipherTest, EncryptDecryptProcess){
 
@@ -40,6 +44,15 @@ TEST(PolyalphabeticCipherTest, EncryptDecryptProcess){
     string plainText = "exampleText";
     string cipherText = polyalphabeticCipher.encrypt(plainText);
     ASSERT_EQ(plainText, polyalphabeticCipher.decrypt(cipherText));
+
+}
+
+TEST(DataEncryptionStandardTest, EncryptDecryptProcess){
+
+    // 8 bytes long plain-text
+    string plainText = "example1";
+    string cipherText = dataEncryptionStandard.encrypt(plainText);
+    ASSERT_EQ(plainText, dataEncryptionStandard.decrypt(cipherText));
 
 }
 
